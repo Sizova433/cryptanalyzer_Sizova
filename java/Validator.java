504c686b58c8fdb88141a704cbb81eb644a0bfcf
Sizova433 <lerochka10005@gmail.com>
2026-05-26 18:50:33 +0300

    Create Validator.java

---
+++ java/Validator.java
@@ -0,0 +1,16 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Validator {
+
+    public static void validate(String pathStr) {
+        Path path = Path.of(pathStr);
+
+        if (!Files.exists(path)) {
+            throw new IllegalArgumentException("Файл не найден по указанному пути: " + pathStr);
+        }
+        if (!Files.isRegularFile(path)) {
+            throw new IllegalArgumentException("Указанный путь ведет не к файлу: " + pathStr);
+        }
+    }
+}
