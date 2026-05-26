3faa5f88dfb514f4be29601a0d10c044e5b8f6e5
Sizova433 <lerochka10005@gmail.com>
2026-05-26 18:48:49 +0300

    Create CaesarCipher.java

---
+++ java/CaesarCipher.java
@@ -0,0 +1,25 @@
+public class CaesarCipher {
+    private static final String RU_UPPER = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+    private static final String RU_LOWER = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+
+    public static char encrypt(char ch, int offset) {
+        int upperIdx = RU_UPPER.indexOf(ch);
+        if (upperIdx != -1) {
+            return shiftCharacter(upperIdx, offset, RU_UPPER);
+        }
+
+        int lowerIdx = RU_LOWER.indexOf(ch);
+        if (lowerIdx != -1) {
+            return shiftCharacter(lowerIdx, offset, RU_LOWER);
+        }
+
+        return ch;
+    }
+
+    private static char shiftCharacter(int currentIdx, int offset, String alphabet) {
+        int length = alphabet.length();
+        // Защита от отрицательного остатка при делении в Java
+        int newIdx = (currentIdx + (offset % length) + length) % length;
+        return alphabet.charAt(newIdx);
+    }
+}
